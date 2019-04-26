201590fe72f8f6014f0489d03d3b9d9296726ef2
Mandy <mandysommerfield@gmail.com>
2019-04-26 14:34:03 -0400

    style home page and navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    color: rgb(230, 230, 250);
+    /* padding-top: 60px; */
+    margin-top: 90px;
+    font-size: 40px;
+    text-shadow: 2px 2px 4px #000000;
+    font-family: 'Berkshire Swash', cursive;
+    p{
+        font-family: 'Capriola', sans-serif;
+        font-size: 25px;
+    }
+    `
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <Wrapper>
                 <h1>Mandy Sommerfield</h1>
                 <p>Full Stack Software Engineer</p>
-            </div>
+            </Wrapper>
         );
     }
 }
 
 export default Home;
+
+
+
+
+// font-family: 'Bitter', serif;
+// font-family: 'Abril Fatface', cursive;
+// font-family: 'Patua One', cursive;
+// font-family: 'Special Elite', cursive;
+// font-family: 'Volkhov', serif;
+// font-family: 'Cabin Condensed', sans-serif;
+// font-family: 'Prata', serif;
+// font-family: 'Cinzel Decorative', cursive;
+// font-family: 'Berkshire Swash', cursive;
+// font-family: 'Yeseva One', cursive;
+// font-family: 'Capriola', sans-serif;
+// font-family: 'Merienda One', cursive;
+
+
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+
 const Wrapper = styled.div`
-    background-color: rgba(4, 44, 61, .5);
+    background-color: rgba(176, 196, 222, .5);
     height: 70px;
-    color: rgb(255, 248, 220);
     text-decoration: none;
     display: flex;
     justify-content: space-around;
 
 
     a {
-    color: rgb(255, 248, 220);
+    color: rgb(230, 230, 250);
     text-decoration: none;
-    font-family: 'IBM Plex Sans', sans-serif;
+    font-family: 'Capriola', sans-serif;
     font-size: 25px;
     cursor: pointer;
     text-shadow: 2px 2px 4px #000000;
